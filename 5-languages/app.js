1c5059ed3bd635d72966abd79641f37b4684b06a
velmo78 <143266246+velmo78@users.noreply.github.com>
2025-05-26 18:52:38 +0300

    5-languages

---
+++ 5-languages/app.js
@@ -0,0 +1,19 @@
+'use strict'
+const language = `he`;
+switch(language){
+    case(`ru`):
+    console.log (`Привет!`)
+    break;
+    case(`en`):
+    console.log (`Hi!`)
+    break;
+    case(`ka`):
+    console.log (`გამარჯობა!`)
+    break;
+    case(`ja`):
+    console.log (`こんにちは !`)
+    break;
+    case(`he`):
+    console.log (`שָלוֹם !`)
+    break;
+}
